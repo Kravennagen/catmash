ccdd5adaced17a4552a9c82d5d25ec79bd2c99d3
Kravennagen <morganeflamant@gmail.com>
2019-02-06 11:01:30 -0800

    using mysql & sending data to mysql OK

--- Api/Controllers/CatMashController.js
+++ Api/Controllers/CatMashController.js
@@ -1,58 +1,53 @@
 'use strict';
 
 var request = require('request');
+var mysql = require('mysql');
 
 exports.get_cats = function(req, res){
-    var params = {
-        "url": "true",
-        "id": "true"
-    };
-
-    const getCats = {
-        uri: "https://latelier.co/data/cats.json",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    };
-
-    request.get(getCats, (error, response, body) =>{
-        if(error){
-            res.status(400).send(error);
-        }
-        var cats = body;
-        console.log("test");
-        console.log(cats.length);
-
-        //console.log(cats);
-        var MongoClient = require('mongodb').MongoClient;
-        var url = "mongodb://localhost:27017/catmash";
-
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            console.log("Database created!");
-            db.close();
-        });
-
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("catmash");
-            dbo.createCollection("cats", function(err, res) {
-              if (err) throw err;
-              console.log("Collection created!");
-              db.close();
-            });
-          });    
-        /*MongoClient.connect(url, function(err, db){
-            if (err) throw err;
-            var dbo = db.db("catmash");
-            dbo.collection("cats").insertMany(, function(err,res){
-                if (err) throw err;
-                console.log("nb inserted: "+ res.insertedCount);
-                db.close()
-            });
-        });*/
-        
-        
-        res.status(200).send({message: 'Cats are here '+ cats});
-    })
+	var params = {
+		"url": "true",
+		"id": "true"
+	};
+	
+	const getCats = {
+		uri: "https://latelier.co/data/cats.json",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	};
+	
+	request.get(getCats, (error, response, body) =>{
+		if(error){
+			res.status(400).send(error);
+		}
+		var cats = JSON.parse(body);
+		
+		//var count = Object.keys(cats.images).length;
+		
+		var con = mysql.createConnection({
+			host: "localhost",
+			user: "catmash",
+			password: "catmash123", 
+			database: "catmash"
+		});
+		con.connect(function(err) {
+			
+					
+			var dict_cat = [];
+			cats.images.forEach(element => {
+				dict_cat.push([element.id, element.url]);
+				
+			});
+			var sql = "INSERT INTO cats (id_cat, url) VALUES ?";
+			var values = dict_cat;
+			con.query(sql, [values], function (err, result) {
+				if (err) throw err;
+				console.log("Number of records inserted: " + result.affectedRows);
+			});
+			
+		});
+		
+		
+		res.status(200).send({message: 'Cats are here '+ cats});
+	})
 }
