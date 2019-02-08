6329b4137c59049efb2fad338b00f8dafc2470b0
Kravennagen <morganeflamant@gmail.com>
2019-02-08 05:50:28 -0800

    formatting folder

--- Catmash/server.js
+++ Catmash/server.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+/*const https = require("https"),
+fs = require("fs");*/
+
 var express = require('express'),
     app = express(),
     port = 3000,
@@ -13,6 +16,10 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
     
+/*const options = {
+    key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
+    cert: fs.readFileSync("/srv/www/keys/chain.pem")
+  };*/
 app.use(allowCrossDomain)
 app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -29,3 +36,4 @@ app.listen(port);
 console.log('Catmash RESTful API server started on: ' + port);
     
 module.exports = app;
+//https.createServer(options, app).listen(8080);
